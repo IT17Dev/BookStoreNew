477ff93c1c865bf476932800149c37d02586d3f6
IT17Dev <119325314+IT17Dev@users.noreply.github.com>
2022-12-02 11:19:28 +0200

    Second commit

---
+++ BookStoreNew/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreNew.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreNew.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Category> Categories { get; set; }
+}
